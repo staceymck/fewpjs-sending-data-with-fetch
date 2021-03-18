2bae9fa0fb2547d9cae8a504c445b4f377518cb8
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-18 14:43:43 -0500

    commit tree 319f9ae62f79d6ee1c808977975d7da2eeaaf93f

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+  let userData = {
+    name: name,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+  
+  return fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(function(obj) {
+      const p = document.createElement('p');
+      p.innerHTML = obj.id;
+      document.body.appendChild(p);
+    })
+    .catch(function(error){
+      const errorBlock = document.createElement('p');
+      errorBlock.innerHTML = error.message;
+      document.body.appendChild(errorBlock)
+    });
+}
+
